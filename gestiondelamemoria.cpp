c855cdb12a0ef1b905011833e379f1d999bbe9d4
Javiersanjuan1 <fsanjua@myuax.com>
2023-12-14 10:43:02 +0100

    Initial commit

--- gestiondelamemoria.cpp
+++ gestiondelamemoria.cpp
@@ -1,7 +1,7 @@
 #include <sys/mman.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>  // Agregamos la inclusión d la cabecera para evitar advertenci
+#include <string.h>  // Agregamos la inclusión d la cabecera para evitar adverten
 #include <unistd.h>
 #include <sys/wait.h>
 
