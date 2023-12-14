a06f8928b761880e34a3c8e8e56487e3bf0ddd59
Javiersanjuan1 <fsanjua@myuax.com>
2023-12-14 10:43:29 +0100

    Initial commit

--- gestiondelamemoria.cpp
+++ gestiondelamemoria.cpp
@@ -1,7 +1,7 @@
 #include <sys/mman.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>  // Agregamos la inclusión d la cabecera para evitar advert
+#include <string.h>  // Agregamos la inclusión d la cabecera para evitar adve
 #include <unistd.h>
 #include <sys/wait.h>
 
