99664dc5c418c0459fae76100defb4849789f1e4
Javiersanjuan1 <fsanjua@myuax.com>
2023-12-14 10:42:32 +0100

    Initial commit

---
+++ gestiondelamemoria.cpp
@@ -0,0 +1,34 @@
+#include <sys/mman.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>  // Agregamos la inclusión de la cabecera para evitar advertencias
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define SIZE 4096
+
+int main() {
+    char *shared_memory = mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    if (shared_memory == MAP_FAILED) {
+        perror("mmap");
+        exit(EXIT_FAILURE);
+    }
+
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        printf("Child reads: %s\n", shared_memory);
+        munmap(shared_memory, SIZE);
+        exit(EXIT_SUCCESS);
+    } else {
+        const char *message = "Hello, child process!";
+        strcpy(shared_memory, message);
+        wait(NULL);
+        munmap(shared_memory, SIZE);
+    }
+
+    return 0;
+}
